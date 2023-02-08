bd0cf276542f2a224e14aa6ff4301a7d5bba3477
Lucas Dauto <lucas.dauto@redacaonline.com.br>
2023-02-08 17:01:59 -0300

    Validator in DTO

--- src/courses/dto/create-courses.dto/create-courses.dto.ts
+++ src/courses/dto/create-courses.dto/create-courses.dto.ts
@@ -1,5 +1,12 @@
+import { IsArray, IsString } from "class-validator";
+
 export class CreateCoursesDto {
+    @IsString()
     readonly name: string;
+
+    @IsString()
     readonly description: string;
+
+    @IsString({  each: true })
     readonly tags: string[];
 }
